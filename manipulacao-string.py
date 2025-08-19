e2d3dde8961ebbe1c2c25f51c56f6b65d4c72011
wall-curso <ltwall73@proton.me>
2025-08-19 20:19:42 -0300

    vamo la, deixa eu dar o push

---
+++ manipulacao-string.py
@@ -0,0 +1,8 @@
+string = 'quem e esse'
+print( string )
+
+print(type(string))
+print(len(string))
+print(string + ' pokemon?')
+substituir = string.replace('quem e esse', 'acredite no coracao das cartas') 
+print(substituir)
