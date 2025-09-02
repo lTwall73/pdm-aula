74245938fe9125523bee4c44d345f5dfda03baaa
Wall <lTwall73@proton.me>
2025-09-02 20:42:49 -0300

    Estrutura condicional e de repetição

---
+++ if-else-elif.py
@@ -0,0 +1,10 @@
+idade = int (20)
+
+if idade >= 18:
+    print('Já pode ser preso.')
+elif idade == int (18):
+    print ('Maioridade agora, não faz merda')
+elif idade < int(18) and idade >= int(14):
+    print ('eca, pré adolescente')
+else:
+    print('Tome seu nescau.')
---
+++ repeticao.py
@@ -0,0 +1,12 @@
+#for
+print ('Exponenciador de numeros!!!!')
+
+
+for numero in range(10):
+    print ( numero ** 2)
+
+#for + lista
+lista_paises = ['Brasil', 'Argentina', 'Uruguai', 'Chile','Paraguai','Bolivia','Equador','Colombia','Suriname','Guiane','Goianai France']
+
+for Loop in lista_paises:
+    print(Loop)
